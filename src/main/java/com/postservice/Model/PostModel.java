40209a49695076098c8dbdd478cf3ef987c52327
Raghvendra Pratap Singh <56296205+007RAGHUSUP@users.noreply.github.com>
2022-03-23 16:45:49 +0530

    Add files via upload

---
+++ src/main/java/com/postservice/Model/PostModel.java
@@ -0,0 +1,35 @@
+package com.postservice.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotEmpty;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "PostService")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class PostModel {
+    @Id
+    private String postID;
+
+    @NotEmpty(message = "user ID is required")
+    private String userID;
+
+    @NotEmpty(message = "post is required")
+    private String post;
+
+    @NotEmpty(message = "postedBy Name is required")
+    private String postedBy;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
